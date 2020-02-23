90d11f2f2d9465c45d587d600d9e58c2c498dc72
Dmitry Churlyaev <tchurlyaev.teba@yandex.com>
2020-02-23 17:56:44 +0300

    eto zhe commit

--- Assets/Scripts/Hex/BasicHexEngine.cs
+++ Assets/Scripts/Hex/BasicHexEngine.cs
@@ -2,21 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public enum RenderState
-{
-    Nothing,
-    Bush,
-    Trees,
-    Animals,
-    Tribe,
-    Village,
-    City,
-    MediumCity,
-    MegaCity,
-    Winner
-}
-
 public abstract class BasicHexEngine : MonoBehaviour
 {
     [SerializeField] protected int neiboursCount = 1;
@@ -25,6 +10,21 @@ public abstract class BasicHexEngine : MonoBehaviour
     [SerializeField] public BasicHexModel hexModel;
     [SerializeField] protected List<BasicHexEngine> hexNeibours = new List<BasicHexEngine>();
     [SerializeField] protected RenderState renderState;
+    public GameObject stones;
+    public GameObject desert;
+    public GameObject clay;
+    public GameObject grass;
+    public GameObject sbirth;
+    public GameObject animal;
+    public GameObject sanimal;
+    public GameObject tribe;
+    public GameObject village;
+    public GameObject smallcity;
+    public GameObject middlecity;
+    public GameObject megapolice;
+
+    [SerializeField] public GameObject tileActive;
+    [SerializeField] public GameObject activeContent;
 
     // Start is called before the first frame update
     void Start()
@@ -38,6 +38,34 @@ public abstract class BasicHexEngine : MonoBehaviour
         hexModel = new BasicHexModel(HexState.Dead);
         getNeibours();
         neiboursCount = hexNeibours.Count;
+        grass = transform.FindDeepChild("HexTop_River").gameObject;
+        desert = transform.FindDeepChild("HexTop_Desert").gameObject;
+        clay = transform.FindDeepChild("HexTop_ClayGround").gameObject;
+        stones = transform.FindDeepChild("HexTop_StoneGround").gameObject;
+
+        sbirth = transform.FindDeepChild("hex_tile_plant1").gameObject;
+        animal = transform.FindDeepChild("hex_tile_trees1").gameObject;
+        sanimal = transform.FindDeepChild("hex_tile_animals2").gameObject;
+        tribe = transform.FindDeepChild("hex_tile_tribe1").gameObject;
+        village = transform.FindDeepChild("hex_tile_vilage2").gameObject;
+        smallcity = transform.FindDeepChild("hex_tile_Small_town1").gameObject;
+        middlecity = transform.FindDeepChild("hex_tile_Middle_town1").gameObject;
+        megapolice = transform.FindDeepChild("hex_tile_Megapolis").gameObject;
+        activeContent = null;
+        tileActive = null;
+        grass.SetActive(false);
+        desert.SetActive(false);
+        clay.SetActive(false);
+        stones.SetActive(false);
+
+        sbirth.SetActive(false);
+        animal.SetActive(false);
+        sanimal.SetActive(false);
+        tribe.SetActive(false);
+        village.SetActive(false);
+        smallcity.SetActive(false);
+        middlecity.SetActive(false);
+        megapolice.SetActive(false);
     }
 
     private void getNeibours()
@@ -68,9 +96,9 @@ public abstract class BasicHexEngine : MonoBehaviour
 
     }
 
-    public virtual void GetRenderState()
+    public virtual RenderState GetRenderState()
     {
-
+        return RenderState.Nothing;
     }
 
     public virtual void Tick()
