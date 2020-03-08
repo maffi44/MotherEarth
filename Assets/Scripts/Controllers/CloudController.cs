0ebeeb1a58da27d9db04543148ca0b93739575c3
Timofei Molokov <48542352+maffi44@users.noreply.github.com>
2020-03-08 16:35:38 +0300

    Theba T (#9)
    Co-authored-by: olesgedz <olesgedz@rambler.ru>

--- Assets/Scripts/Controllers/CloudController.cs
+++ Assets/Scripts/Controllers/CloudController.cs
@@ -15,38 +15,43 @@ public class CloudController : EnvController
         hexes = GameObject.FindGameObjectsWithTag("Hex");
         planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<PlanetController>();
         clouds = GameObject.FindGameObjectsWithTag("Cloud");
+        foreach (GameObject cloud in clouds)
+        {
+            //Debug.Log(hex.GetComponent<BasicHexEngine>().IsAlive());
+            Debug.Log(cloud.gameObject.name);
+            Cloud cloudObj = cloud.GetComponent<Cloud>();
+            GameObject obj = cloudObj.castRay();
+            if (obj != null)
+            {
+                BasicHexEngine hex = obj.GetComponent<BasicHexEngine>();
+                if (hex != null)
+                {
+                    cloudObj.attachedHex = hex;
+                    hex.attachedCloud = cloudObj;
+                }
+            }
+
+        }
     }
 
     public override void Execute()
     {
         if (base.IsRun())
         {
-            //if (spawn == true)
-            //{
-            //    this.SpawnCloud();
-            //    spawn = false;
-            //}
-
             foreach (GameObject cloud in clouds)
             {
-                //Debug.Log(hex.GetComponent<BasicHexEngine>().IsAlive());
-                Debug.Log(cloud.gameObject.name);
-                GameObject obj = cloud.GetComponent<Cloud>().castRay();
-                if (obj != null)
-                {
-                    Debug.Log(obj.name);
-                }
+                Cloud cloudObj = cloud.GetComponent<Cloud>();
+                cloudObj.attachedHex.hexModel.SetState(HexState.Alive);
+                cloudObj.attachedHex.hexModel.SetProgressState(ProgressState.Forest);
+            }
+        }
 
-            //cloud.GetComponent<Cloud>().castRay().gameObject.GetComponent<IHexEngine>().Live();
-        }
-        }
+        //public void SpawnCloud()
+        //{
+        //    Debug.Log("New cloud");
+        //    GameObject newCloud = Instantiate(cloudPreb, planet.transform.position - new Vector3(4,4,4), Quaternion.identity) as GameObject;
+        //    newCloud.transform.LookAt(planet.transform.position);
+        //    clouds.Add(newCloud);
+        //}
     }
-
-    //public void SpawnCloud()
-    //{
-    //    Debug.Log("New cloud");
-    //    GameObject newCloud = Instantiate(cloudPreb, planet.transform.position - new Vector3(4,4,4), Quaternion.identity) as GameObject;
-    //    newCloud.transform.LookAt(planet.transform.position);
-    //    clouds.Add(newCloud);
-    //}
 }
