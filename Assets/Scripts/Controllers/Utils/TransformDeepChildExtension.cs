3d2dcb81a56e3f2129c6229ef6bf88945ae36647
Dmitry Churlyaev <tchurlyaev.teba@yandex.com>
2020-03-09 18:48:59 +0300

    lll

--- Assets/Scripts/Controllers/Utils/TransformDeepChildExtension.cs
+++ Assets/Scripts/Controllers/Utils/TransformDeepChildExtension.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
-public class TDeepChildFinder<T, K> where T : IEnumerable
+
+public static class TransformDeepChildExtension
 {
-    //Breadth-first search
-    public T FindDeepChild(T aParent, string aFieldLink, K aField)
+    //Breadth-first search	    //Breadth-first search
+    public static Transform FindDeepChild(this Transform aParent, string aName)
     {
-        Queue<T> queue = new Queue<T>();
+        Queue<Transform> queue = new Queue<Transform>();
         queue.Enqueue(aParent);
         while (queue.Count > 0)
         {
             var c = queue.Dequeue();
-            if (aParent.GetType().GetField(aFieldLink, BindingFlags.NonPublic | BindingFlags.Instance).Equals(aField))
-                return c;
-            foreach (T t in c)
-                queue.Enqueue(t);
+            if (c.name == aName)
+                    return c;
+            foreach (Transform t in c)
+                    queue.Enqueue(t); 
         }
-        return default(T);
+        return null; 
     }
 }
